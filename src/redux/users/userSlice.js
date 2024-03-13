1100cf6848caff537832fe18c38369ee9b7638b4
ben64103 <benabolade@gmail.com>
2024-03-13 12:28:52 +0100

    added auth folder for auth

--- src/redux/users/userSlice.js
+++ src/redux/users/userSlice.js
@@ -1,9 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const user = {
+  name: '',
+  email: '',
+  password: '',
+  profilePic: '',
+  confirmPassword: '',
+  accountType: '',
+};
+
+const initialState = {
+  status: 'idle',
+  users: [],
+  error: null,
+};
+
 const usersSlice = createSlice({
   name: 'users',
-  initialState: [],
-  reducers: {},
+  initialState,
+  reducers: {
+    updateAcctType(state, action) {
+      state.users.push({ ...user, accountType: action.payload });
+    },
+  },
 });
 
 export default usersSlice.reducer;
