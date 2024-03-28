a154ea3effc08807a478af0451be9abcb04c3fe5
ben64103 <benabolade@gmail.com>
2024-03-28 15:38:31 +0100

    ...done with part navigatio, error: broken svgs

--- src/components/NavigationBar/NavbarEnd.jsx
+++ src/components/NavigationBar/NavbarEnd.jsx
@@ -4,18 +4,18 @@ const NavbarEnd = () => {
   return (
     <div className='navbar-end flex items-center transition-all'>
       <div className='relative w-80'>
-        <div
-          id='icon'
-          className='absolute search-icon inset-y-0 start-0 flex items-center ps-3 text-gray-400 pointer-events-none peer-focus/icon:text-red-600'>
-          <SearchIcon />
-          <span className='sr-only'>Search icon</span>
-        </div>
         <input
           id='search'
           type='search'
-          className='block w-full p-1.5 ps-12 text-sm peer/search bg-gray-100 border border-gray-100 rounded-full outline-none focus:bg-white focus:border focus:border-gray-300 focus:shadow-s'
+          className='peer block w-full p-1.5 ps-12 text-sm bg-gray-100 border border-gray-100 rounded-full outline-none focus:bg-white focus:border focus:border-gray-300 focus:shadow-sm'
           placeholder='Search'
         />
+        <div
+          id='icon'
+          className='absolute search-icon inset-y-0 start-0 flex items-center ps-3 text-gray-400 pointer-events-none peer-focus:text-blue-600'>
+          <SearchIcon />
+          <span className='sr-only'>Search icon</span>
+        </div>
       </div>
     </div>
   );
--- src/components/NavigationBar/NavbarStart.jsx
+++ src/components/NavigationBar/NavbarStart.jsx
@@ -9,17 +9,19 @@ import Icon from '@mdi/react';
 
 const NavbarStart = () => {
   const navIconButtons = [
-    mdiApps,
-    mdiMailbox,
-    mdiBellOutline,
     mdiHeartOutline,
+    mdiBellOutline,
+    mdiMailbox,
     mdiMessageOutline,
+    mdiApps,
   ];
 
   return (
-    <div className='navbar-start flex items-center gap-8 *:text-gray-500'>
+    <div className='navbar-start flex items-center gap-8 *:text-gray-500 *:h-9 *:w-9 *:rounded-md '>
       {navIconButtons.map((icon, i) => (
-        <button key={i}>
+        <button
+          key={i}
+          className='inline-flex-center hover:bg-blue-700 hover:text-white hover:first-of-type:bg-red-400'>
           <Icon path={icon} size={0.9} />
         </button>
       ))}
