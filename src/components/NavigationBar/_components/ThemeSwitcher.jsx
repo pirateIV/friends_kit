31986e18d8b172521c522b8dafb355f75d3a3fbf
ben64103 <benabolade@gmail.com>
2024-04-07 21:17:58 +0100

    working on the toggle switch profile page

--- src/components/NavigationBar/_components/ThemeSwitcher.jsx
+++ src/components/NavigationBar/_components/ThemeSwitcher.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTheme, setTheme } from '@/redux/reducers/themeReducer';
 import DarkModeIcon from '@/shared/components/icons/DarkModeIcon';
@@ -11,26 +11,39 @@ const ThemeSwitcher = () => {
     setIsDarkMode(!isDarkMode);
   };
 
-
   const theme = useSelector(getTheme());
   const dispatch = useDispatch();
+  useEffect(() => {
+    toggleDarkMode();
+  }, [theme]);
 
   const selectedTheme = theme === 'dark' ? 'light' : 'dark';
   const changePreference = () => {
     dispatch(setTheme(selectedTheme));
   };
-  
 
   return (
     <div id='theme-switcher'>
-      <label className='animated-toggle flex items-center cursor-pointer'>
+      <label
+        className={`animated-toggle flex items-center border border-gray-700 h-6 w-[46px] p-0.5 rounded-full cursor-pointer`}>
         <input
           type='checkbox'
+          className='hidden'
           checked={selectedTheme}
           onChange={changePreference}
-          className='hidden'
         />
-        <span className='toggler w-12 h-6 rounded-full border-2 border-gray-300 relative'>
+        <span>
+          <div
+            className={`text-white h-5 w-5 flex-center p-1 rounded-full ${
+              isDarkMode
+                ? 'bg-blue-500 translate-x-full rotate-0'
+                : 'bg-yellow-800 translate-x-0 rotate-180'
+            }`}
+            style={{ transition: '0.3s ease' }}>
+            {!isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+          </div>
+        </span>
+        {/* <span className='toggler w-12 h-6 rounded-full border-2 border-gray-300 relative'>
           <span
             className={`absolute left-0 w-6 h-6 rounded-full bg-white flex items-center justify-center transition-transform duration-300 ${
               isDarkMode ? 'translate-x-full' : 'translate-x-0'
@@ -43,7 +56,7 @@ const ThemeSwitcher = () => {
             }`}>
             <LightModeIcon />
           </span>
-        </span>
+        </span> */}
       </label>
     </div>
   );
