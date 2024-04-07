fabb85a5a243ceb3cf58c8b95f85c2f644eef80b
ben64103 <benabolade@gmail.com>
2024-04-07 21:22:51 +0100

    finished toggle functionlaity

--- src/components/NavigationBar/_components/ThemeSwitcher.jsx
+++ src/components/NavigationBar/_components/ThemeSwitcher.jsx
@@ -7,14 +7,14 @@ import LightModeIcon from '@/shared/components/icons/LightModeIcon';
 const ThemeSwitcher = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  // const toggleDarkMode = () => {
+  //   setIsDarkMode(!isDarkMode);
+  // };
 
   const theme = useSelector(getTheme());
   const dispatch = useDispatch();
   useEffect(() => {
-    toggleDarkMode();
+    theme === 'dark' ? setIsDarkMode(true) : setIsDarkMode(false);
   }, [theme]);
 
   const selectedTheme = theme === 'dark' ? 'light' : 'dark';
@@ -25,7 +25,9 @@ const ThemeSwitcher = () => {
   return (
     <div id='theme-switcher'>
       <label
-        className={`animated-toggle flex items-center border border-gray-700 h-6 w-[46px] p-0.5 rounded-full cursor-pointer`}>
+        className={`animated-toggle flex items-center border h-6 w-[46px] p-0.5 rounded-full cursor-pointer ${
+          isDarkMode ? 'border-blue-500' : 'border-gray-700'
+        }`}>
         <input
           type='checkbox'
           className='hidden'
@@ -34,29 +36,14 @@ const ThemeSwitcher = () => {
         />
         <span>
           <div
-            className={`text-white h-5 w-5 flex-center p-1 rounded-full ${
+            className={`text-white h-5 w-5 flex-center p-1 rounded-full transition-03 ${
               isDarkMode
                 ? 'bg-blue-500 translate-x-full rotate-0'
                 : 'bg-yellow-800 translate-x-0 rotate-180'
-            }`}
-            style={{ transition: '0.3s ease' }}>
+            }`}>
             {!isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
           </div>
         </span>
-        {/* <span className='toggler w-12 h-6 rounded-full border-2 border-gray-300 relative'>
-          <span
-            className={`absolute left-0 w-6 h-6 rounded-full bg-white flex items-center justify-center transition-transform duration-300 ${
-              isDarkMode ? 'translate-x-full' : 'translate-x-0'
-            }`}>
-            <DarkModeIcon />
-          </span>
-          <span
-            className={`absolute right-0 w-6 h-6 rounded-full bg-white flex items-center justify-center transition-transform duration-300 ${
-              isDarkMode ? 'translate-x-0' : '-translate-x-full'
-            }`}>
-            <LightModeIcon />
-          </span>
-        </span> */}
       </label>
     </div>
   );
