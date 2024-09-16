77ee899ae8c06015a2b1539de1da0a5ac8140c0c
pirateIV <benabolade@gmail.com>
2024-09-16 19:24:58 +0100

    chore: update backend url

--- src/api/client.js
+++ src/api/client.js
@@ -1,1 +1,4 @@
-export const baseURL = "http://localhost:5000/api";
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://friends-kit-backend.onrender.com/api"
+    : "http://localhost:5000/api";
