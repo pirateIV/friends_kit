ddae76c2c9d2a034bfb7c3257368496b0eea36a1
pirateIV <benabolade@gmail.com>
2024-09-20 10:27:10 +0100

    refactor: cleanup css, update chat ui

--- src/pages/Chat/components/ChatBubble.jsx
+++ src/pages/Chat/components/ChatBubble.jsx
@@ -5,21 +5,23 @@ import { formatTime } from "@/helpers/formatDate";
 const ChatBubble = (props) => {
   const { message, sender, timestamp } = props.message;
 
+  const messageFromSelf = sender === socket.auth.userID;
+
   return (
     <div
       className={cn(
         "grid grid-cols-2 gap-3 text-white",
-        socket.auth.userID === sender
-          ? "place-items-start col-start-1 grid-cols-[auto_1fr]"
-          : "place-items-end col-start-2 grid-cols-[1fr_auto]",
+        messageFromSelf
+          ? "place-items-end col-start-2 grid-cols-[1fr_auto]"
+          : "place-items-start col-start-1 grid-cols-[auto_1fr]",
       )}
     >
       <div
         className={cn(
-          "relative ps-5 px-4 pt-2 pb-3 border-t rounded-lg text-sm mb-2 shadow-sm shadow-gray-600 hover:opacity-95 dark:shadow-none",
-          socket.auth.userID === sender
-            ? "min-w-32 bg-slate-500 border-slate-400"
-            : "bg-[#3d70b2] border-[#558cd4]",
+          "relative pt-1.5 ps-4 pb-1 pe-20 border-t rounded-lg text-[13px] mb-2 shadow-sm shadow-gray-600 hover:opacity-95 dark:shadow-none",
+          messageFromSelf
+            ? "bg-[#3d70b2] border-[#558cd4]"
+            : "min-w-32 bg-slate-500 border-slate-400",
         )}
       >
         {message.map((content, i) => (
@@ -28,7 +30,7 @@ const ChatBubble = (props) => {
           </div>
         ))}
         <time
-          className="absolute end-5 bottom-1 text-xs text-end opacity-70 font-serif"
+          className="absolute end-4 bottom-0.5 text-[11px] text-end opacity-70"
           dateTime={formatTime(timestamp)}
         >
           {formatTime(timestamp)}
